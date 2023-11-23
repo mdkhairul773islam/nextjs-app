0f4ad41d9dff5a21f04d118695052d271c62e43c
Khairul islam <mdkhairul773@gmail.com>
2023-11-23 10:45:18 +0600

    using npx prisma db pull

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
 /** @type {import('next').NextConfig} */
-const { authMiddleware } = require('./authMiddleware');
 const nextConfig = {}
-
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/protected',
-          destination: '/protected',
-          has: [
-            {
-              type: 'cookie',
-              key: 'token',
-              value: '.+', // Match any value (non-empty token)
-            },
-          ],
-          middleware: authMiddleware,
-        },
-        // Add more rewrite rules as needed for other protected routes
-      ];
-    },
-  };
 module.exports = nextConfig
