f991ff7812e165740f3359adb956bafde9c2aca6
mdkhairul773islam <mdkhiarul773@gmail.com>
2023-11-25 19:49:08 +0600

    optimized code

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
         <h3 className="text-2xl font-bold mb-5">Featured Destinations</h3>
         <div className="grid grid-cols-3 gap-4">
           {/* Destination cards will go here */}
+
         </div>
       </div>
       <Footer />
