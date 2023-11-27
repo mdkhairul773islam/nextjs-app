bcf9c853e1ac0bb19511ac6550b4c8044f11eaaf
mdkhairul773islam <mdkhiarul773@gmail.com>
2023-11-27 08:36:58 +0600

    try to file upload

---
+++ src/app/fileupload/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FileUploader from './FileUploader'
+export default function FileUpload() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">File Upload</h1>
+            <FileUploader />
+        </div>
+    )
+}
+
