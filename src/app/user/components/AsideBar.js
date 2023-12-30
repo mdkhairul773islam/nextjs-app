74fb337371571f553d1a0f51c7bd157f805710dd
mdkhairul773islam <mdkhairul773@gmail.com>
2023-12-30 14:55:58 +0600

    make Submenu Component

--- src/app/user/components/AsideBar.js
+++ src/app/user/components/AsideBar.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faAngleDown, faHome, faList, faCog } from '@fortawesome/free-solid-svg-icons';
-
+import { faHome, faList, faCog } from '@fortawesome/free-solid-svg-icons';
+import SubMenu from './SubMenu';
 const AsideBar = ({ isAsideVisible }) => {
     const [submenuStates, setSubmenuStates] = useState(new Array(6).fill(false));
-
     const toggleSubmenu = (index) => {
         const newSubmenuStates = [...submenuStates];
         newSubmenuStates[index] = !newSubmenuStates[index];
         setSubmenuStates(newSubmenuStates);
     };
-
     return (
         <aside className={`${isAsideVisible ? 'w-64' : 'w-25'} h-screen dark:bg-white text-gray-900 p-5`}>
             <ul className="cursor-pointer select-none">
@@ -25,48 +23,21 @@ const AsideBar = ({ isAsideVisible }) => {
                     <FontAwesomeIcon icon={faList} className="mr-2"/>
                     Link 2
                 </li>
-                <li
-                    className="mb-4 flex flex-col"
-                >
-                    <div
-                        className="flex items-center justify-between mb-2"
-                        onClick={() => toggleSubmenu(0)}
-                    >
-                        <div className="flex items-center">
-                            <FontAwesomeIcon icon={faCog} className="mr-2"/>
-                            Link 3
-                        </div>
-                        <FontAwesomeIcon icon={submenuStates[0] ? faAngleDown : faAngleRight} className="ml-2"/>
-                    </div>
-                    {submenuStates[0] && (
-                        <ul className="pl-4 transition-all duration-300">
-                            <li className="mb-2">Submenu 1</li>
-                            <li className="mb-2">Submenu 2</li>
-                        </ul>
-                    )}
-                </li>
-                <li
-                    className="mb-4 flex flex-col"
-                >
-                    <div
-                        className="flex items-center justify-between mb-2"
-                        onClick={() => toggleSubmenu(1)}
-                    >
-                        <div className="flex items-center">
-                            <FontAwesomeIcon icon={faCog} className="mr-2"/>
-                            Link 4
-                        </div>
-                        <FontAwesomeIcon icon={submenuStates[1] ? faAngleDown : faAngleRight} className="ml-2"/>
-                    </div>
-                    {submenuStates[1] && (
-                        <ul className="pl-4 transition-all duration-300">
-                            <li className="mb-2">Submenu 1</li>
-                            <li className="mb-2">Submenu 2</li>
-                            {/* Add other submenu items here */}
-                        </ul>
-                    )}
-                </li>
+                <SubMenu
+                    icon={faCog}
+                    label="Link 3"
+                    submenuItems={['Submenu 1', 'Submenu 2']}
+                    isOpen={submenuStates[0]}
+                    onClick={() => toggleSubmenu(0)}
+                />
 
+                <SubMenu
+                    icon={faCog}
+                    label="Link 4"
+                    submenuItems={['Submenu 1', 'Submenu 2']}
+                    isOpen={submenuStates[1]}
+                    onClick={() => toggleSubmenu(1)}
+                />
                 {/* Repeat the above block for links 5 to 8 */}
             </ul>
         </aside>
---
+++ src/app/user/components/SubMenu.js
@@ -0,0 +1,26 @@
+// SubMenu.js
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+const SubMenu = ({ icon, label, submenuItems, isOpen, onClick }) => (
+    <li className="mb-4 flex flex-col">
+        <div className="flex items-center justify-between mb-2" onClick={onClick}>
+            <div className="flex items-center">
+                <FontAwesomeIcon icon={icon} className="mr-2" />
+                {label}
+            </div>
+            <FontAwesomeIcon icon={isOpen ? faAngleDown : faAngleRight} className="ml-2" />
+        </div>
+        {isOpen && (
+            <ul className="pl-4 transition-all duration-300">
+                {submenuItems.map((item, index) => (
+                    <li key={index} className="mb-2">
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        )}
+    </li>
+);
+
+export default SubMenu;
